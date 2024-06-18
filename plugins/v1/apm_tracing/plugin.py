6447f0053c91f80292d5372ce755bd06d1377be2
favilo <kevin.oberlies@elastic.co>
2024-06-18 12:05:47 -0700

    Add `apm_tracing` plugin to allow us to enable APM Tracing on es nodes
    [APM Tracing][1]
    
    This needs to be used in conjunction with setting
    `"additional_cluster_settings"` in `car-params`
    to include:
    
    ```
    {
        telemetry.tracing.enabled: true,
        telemetry.agent.server_url: https://<your-apm-server>:443
    }
    ```
    
    [1]: https://github.com/elastic/elasticsearch/blob/main/TRACING.md#how-is-tracing-configured

---
+++ plugins/v1/apm_tracing/plugin.py
@@ -0,0 +1,94 @@
+import logging
+import os
+import shlex
+import subprocess
+
+from esrally import exceptions
+from esrally.utils import process
+
+LOGGER_NAME = "esrally.provisioner.apm_tracing"
+
+
+def resolve_binary(install_root, binary_name):
+    return os.path.join(install_root, "bin", binary_name)
+
+
+def resolve_keystore_config(install_root):
+    return os.path.join(install_root, "config", "elasticsearch.keystore")
+
+
+def create_keystore(install_root, keystore_binary, env):
+    logger = logging.getLogger(LOGGER_NAME)
+
+    keystore_create_command = "{keystore} -s create".format(keystore=keystore_binary)
+
+    return_code = process.run_subprocess_with_logging(keystore_create_command, env=env)
+
+    if return_code != 0:
+        logger.error(
+            "%s has exited with code [%d]", keystore_create_command, return_code
+        )
+        raise exceptions.SystemSetupError(
+            "Could not initialize a keystore. Please see the log for details."
+        )
+
+
+def add_property_to_keystore(
+    keystore_binary, client_name, property_name, property_value, env
+):
+    logger = logging.getLogger(LOGGER_NAME)
+
+    p1 = subprocess.Popen(["echo", property_value], stdout=subprocess.PIPE)
+
+    keystore_command = (
+        "{keystore} --silent add --stdin s3.client.{client_name}.{key}".format(
+            keystore=keystore_binary, client_name=client_name, key=property_name
+        )
+    )
+
+    return_code = process.run_subprocess_with_logging(
+        keystore_command, stdin=p1.stdout, env=env
+    )
+
+    if return_code != 0:
+        logger.error("%s has exited with code [%d]", keystore_command, return_code)
+        raise exceptions.SystemSetupError(
+            "Could not add S3 keystore secure setting [{}]. Please see the log for details.".format(
+                property_name
+            )
+        )
+
+
+def configure_keystore(config_names, variables, **kwargs):
+    logger = logging.getLogger(LOGGER_NAME)
+    secret_token = variables.get("apm_secret_token")
+
+    # skip keystore configuration entirely if any of the mandatory params is missing
+    if not secret_token:
+        logger.warning(
+            "Skipping keystore configuration for apm-tracing "
+            "as mandatory plugin-params [`apm_secret_token`] was not supplied",
+        )
+        return False
+
+    keystore_binary_filename = "elasticsearch-keystore"
+    install_root = variables["install_root_path"]
+    keystore_binary = resolve_binary(install_root, keystore_binary_filename)
+    env = kwargs.get("env")
+
+    if not os.path.isfile(resolve_keystore_config(install_root)):
+        create_keystore(install_root, keystore_binary, env)
+
+    es_property_name = "telemetry.secret_token"
+    # skip optional properties like session_token
+
+    add_property_to_keystore(
+        keystore_binary, client_name, es_property_name, secret_token, env
+    )
+
+    # Success
+    return True
+
+
+def register(registry):
+    registry.register("post_install", configure_keystore)
